c6a7fa2a570152f1b4ac5b7a842d7f1e3bbf7f70
Klaabut <54986407+Klaabut@users.noreply.github.com>
2020-01-29 15:16:56 +0200

    1st commit

---
+++ Ühis-Transpordi-Agregaat/Ühis-Transpordi-Agregaat/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Ühis_Transpordi_Agregaat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string transportListUrl = ("https://transport.tallinn.ee/gps.txt");
+            List<string> listOfVehicles = new List<string>();
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(transportListUrl);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+            using(var responseReader = new StreamReader(webStream))
+            {
+                string response = responseReader.ReadToEnd();
+                listOfVehicles.Add(response);
+                
+            }
+
+           /* foreach(string line in listOfVehicles)
+            {
+                Console.WriteLine(line);
+            }*/
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ühis-Transpordi-Agregaat/Ühis-Transpordi-Agregaat/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ühis_Transpordi_Agregaat
+{
+    class Vehicle
+    {
+        public string type;
+        public string lineNr;
+        public string langitude;
+        public string latitude;
+
+        public void VehicleConstructor(string _type, string _lineNr, string _langitude, string _latitude)
+        {
+            type = _type;
+            lineNr = _lineNr;
+            langitude = _langitude;
+            latitude = _latitude;
+        }
+
+    }
+}
